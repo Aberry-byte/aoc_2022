2ea90403f2d45ecf580ddb97133a89394c89f304
Alex H <alexhuft@gmail.com>
2022-12-18 11:38:47 -0700

    part 1 of day 3

--- Day03/src/main/kotlin/Main.kt
+++ Day03/src/main/kotlin/Main.kt
@@ -2,10 +2,37 @@
 Author: Alex Huft
 Description: Advent of Code 2022 Day 3
  */
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+fun getPriority(c: Char?): Int {
+    val asciiVal = c!!.code
+    if (asciiVal < 91) {
+        return asciiVal - (65 - 27)
+    }
+    else {
+        return asciiVal - 96
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val input = "input.txt"
+    // val input = "short_input.txt"
+    val file = File(input)
+    val lines = file.readLines()
+    var totalPriority = 0
+    for (line in lines) {
+        var similarChar: Char? = null
+        val firstHalf = line.substring(0, line.length/2)
+        val secondHalf = line.substring(line.length/2, line.length)
+        for (c in firstHalf) {
+            if (secondHalf.contains(c)) {
+                similarChar = c
+                break
+            }
+        }
+        totalPriority += getPriority(similarChar)
+    }
+    println(totalPriority)
 }
