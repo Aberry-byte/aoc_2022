c900549dabe04638d135373cf1a2871482957064
Alex H <alexhuft@gmail.com>
2022-12-18 23:46:09 -0700

    skipping day 5 for now

---
+++ Day05/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+/*
+Author: Alex Huft
+Description: Advent of Code day 5
+ */
+import java.io.File
+import java.util.Stack
+
+
+fun main(args: Array<String>) {
+    // val input = "input.txt"
+    val input = "small_input.txt"
+    val file = File(input)
+    val stacks: ArrayList<Stack<Char>> = ArrayList()
+}
